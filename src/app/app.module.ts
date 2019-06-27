c00707fd8632364541dca207be4539ece0fb1c69
jose <joseantoniolandete@gmail.com>
2019-06-27 14:29:13 +0200

    dunno

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 
@@ -13,9 +11,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
 
   ],
   providers: [],
