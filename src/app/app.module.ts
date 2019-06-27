10d19df72bef014a2f8e95a7a3d1c84bcb9009a4
jose <joseantoniolandete@gmail.com>
2019-06-27 15:54:03 +0200

    fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MegaMenuModule} from 'primeng/megamenu';
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FileUploadModule} from 'primeng/fileupload';
+
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import {DropdownModule} from 'primeng/dropdown';
+import {ButtonModule} from 'primeng/button';
+
+
 
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent
   ],
   imports: [
     AppRoutingModule,
+    MegaMenuModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    FileUploadModule,
+    DropdownModule,
+    ButtonModule
 
   ],
   providers: [],
